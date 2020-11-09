61de320b84aec04e6d65fe2ddf0f658668ad12fc
Nitinjmv <nitinjmv@gmail.com>
2020-11-10 03:58:38 +0530

    intial commit

---
+++ src/main/java/com/jmv/HelloworldApplication.java
@@ -0,0 +1,27 @@
+package com.jmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
+
+
+@RestController
+@RequestMapping("/")
+class Helloworld {
+	
+	@GetMapping	
+	public String hello() {
+		return "Hello World";
+	}
+}
